88f7e3b1d3abe26b049324f27d1c74594e1893ad
Gael Santos <jaffary21gball360@gmail.com>
2025-08-25 14:45:10 -0400

    This is assignment 1

---
+++ LSP_Assignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Gael Santos
+ * 
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
